da22c92d776af14dacea8676688dda28e9616b04
github username <github email address>
2024-10-01 13:09:02 -0400

    Initial commit

---
+++ src/com/mac286/project/Helper.java
@@ -0,0 +1,29 @@
+package com.mac286.project;
+
+import java.util.Vector;
+
+public class Helper {
+    //Write a static method that accepts a path and a file name
+    //it opend the file (file symbols) and reads it and creates a
+    //Vector of strings of all files.
+    public static Vector<String> loadSymbols(String path, String file){
+        //TODO: Create a Vector symbols
+        //Open the input file and read line by line, trim the string
+        //and add it to the Vector symbols
+        //return the vector
+        return null;
+    }
+
+    //Write a Static method that accepts a Vector of Trades and goes
+    //through it to compute all statistics, return the statistics as
+    //an object.
+    public Statistics computeStats(Vector<Trade> trades){
+        //Create a Statistics object
+
+        //go through Vector trades one by one and compute all the Stats
+
+        //return your object.
+
+        return null;
+    }
+}
---
+++ src/com/mac286/project/Main.java
@@ -0,0 +1,26 @@
+package com.mac286.project;
+/*
+In this class you desing you main to test All stocks and
+ETFs
+1- create a Tester with the appropriate path and file name and risk factor
+2- Call run
+3- Get the trades
+4- Call helper function that will compute all statistics from the vector trades
+ */
+public class Main {
+    public static void main(String[] args) {
+        //If you are testing a risk based on stoploss and target
+        float[] riskFactor = {0.5f, 1f, 2f, 5f, 10f};
+        //set path to appropriate path ("C:/ProfOmarMAC286/Spring2024/Data/)
+        //set file to appropriate file (stocks.txt)
+
+        //loop through your risk array and do the following
+        // Create a Tester with with path file and riskFactor[i]
+        //Call run method on the tester
+        // get the trades vector getTrades()
+        //call the helper method computerStates with the trade vector as input
+        //display the results using the toString of the Statistics method
+
+        //Change the filename to ETFs.txt and do the same.
+    }
+}
