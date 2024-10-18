af3bfd6aa2a42728025bceee0383b8d6362664d6
Paul Gutierrez <paulitoguti747@gmail.com>
2024-10-17 22:11:17 -0400

    Create loadSymbols static method in Helper

--- src/com/mac286/project/Helper.java
+++ src/com/mac286/project/Helper.java
@@ -1,17 +1,32 @@
 package com.mac286.project;
 
 import java.util.Vector;
+import java.io.FileReader;
+import java.io.*;
 
 public class Helper {
     //Write a static method that accepts a path and a file name
-    //it opend the file (file symbols) and reads it and creates a
+    //it opens the file (file symbols) and reads it and creates a
     //Vector of strings of all files.
+    // make sure to pass in: "src/com/mac286/project/Data/" as the path when using loadSymbols()
     public static Vector<String> loadSymbols(String path, String file){
         //TODO: Create a Vector symbols
+        Vector<String> symbols = new Vector<String>();
         //Open the input file and read line by line, trim the string
         //and add it to the Vector symbols
+        File currFile = new File(path+file);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(currFile));
+            // read file line by line
+            String st;
+            while ((st = br.readLine()) != null) {
+                symbols.add(st);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         //return the vector
-        return null;
+        return symbols;
     }
 
     //Write a Static method that accepts a Vector of Trades and goes
