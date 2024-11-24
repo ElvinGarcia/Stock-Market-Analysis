fb041d1a315764eca65e431a9a17dedda323eca9
Paul Gutierrez <paulitoguti747@gmail.com>
2024-11-23 22:34:58 -0500

    Clean up Main.java for better readability
    Added and edited comments where needed, and switched around where things
    are so that it's easier for the programmer to read, and for better
    visibility of the project results in the terminal.

--- src/com/mac286/project/Main.java
+++ src/com/mac286/project/Main.java
@@ -11,54 +11,50 @@ In this class you'll design a main to test all stocks and ETFs
  */
 public class Main {
     public static void main(String[] args) {
-        // If you are testing a risk based on stop-loss and target
+        // if you are testing a risk based on stop-loss and target
         float[] riskFactor = {0.5f, 1f, 2f, 5f, 10f};
         //set path to appropriate path
         String path = "src/com/mac286/project/Data/";
         //set file to appropriate file
         String fileStocks = "Stocks.txt", fileETFs = "ETFs.txt";
 
-
-
         //loop through the risk array
         for (float v : riskFactor) {
+            System.out.printf("\n************* Current Risk: %.1f *************\n\n", v);
 
-
-            System.out.printf("\n ************* Current Risk: %f *************", v);
-            // Create a Tester with arguments path, file, and riskFactor v
+            // Calculate the stats for Stocks:
+            // create a Tester with arguments path, file, and riskFactor v
             Tester testerStock = new Tester(path, fileStocks, v);
-            //Call run method on the tester
+            // call run method on the tester
             testerStock.run();
             // get the trades vector getTrades()
             Vector<Trade> tradesStock = testerStock.getTrades();
-            //call the helper method computeStats with the trade vector as input
+            // call the helper method computeStats with the trade vector as input
             Statistics statsStock = Helper.computeStats(tradesStock);
 
-
-            //display the results using the toString of the Statistics method
+            // display the results using the toString of the Statistics method
             System.out.println("************* Stock Statistics *************");
-
-            System.out.println("\n Number of Stocks "+ tradesStock.size() + "\n");
-
             System.out.println(statsStock);
+
+            // Calculate the stats for ETFs:
+            // create a Tester with arguments path, file, and riskFactor v
             Tester testerETF = new Tester(path, fileETFs, v);
+            // call run method on the tester
             testerETF.run();
+            // get the trades vector getTrades()
             Vector<Trade> tradesETF = testerETF.getTrades();
-
-
-
-            System.out.println("\n************* ETF Statistics *************");
-
-            System.out.println("\n Number of ETF "+ tradesETF.size() + "\n");
-
+            // call the helper method computeStats with the trade vector as input
             Statistics statsETF = Helper.computeStats(tradesETF);
 
+            // display the results using the toString of the Statistics method
+            System.out.println("\n************* ETF Statistics *************");
             System.out.println(statsETF);
 
+            // Calculate the Combined Stats:
             tradesStock.addAll(tradesETF);
             Statistics all = Helper.computeStats(tradesStock);
 
-
+            // display the results using the toString of the Statistics method
             System.out.println("\n************* Combined Statistics *************");
             System.out.println(all);
         }
