e93948d37bca37b9138d8b8aa42a051ba5288be5
Paul Gutierrez <paulitoguti747@gmail.com>
2024-11-23 22:37:44 -0500

    Edit Statistics toString for better visibility
    Edited the toString method in Statistics.java for better visibility
    of its attributes within the Main.java file. Information is now more
    clear, and visually appealing.

--- src/com/mac286/project/Statistics.java
+++ src/com/mac286/project/Statistics.java
@@ -17,19 +17,20 @@ public class Statistics {
 
     @Override
     public String toString() {
-        return "Statistics{" +
-                "averageProfit=" + averageProfit +
-                ", averageHoldingPeriod=" + averageHoldingPeriod +
-                ", averageProfitPerDay=" + averageProfitPerDay +
-                ", winningPercent=" + winningPercent + "\n" +
-                ", averageProfitLong=" + averageProfitLong +
-                ", averageHoldingPeriodLong=" + averageHoldingPeriodLong +
-                ", averageProfitPerDayLong=" + averageProfitPerDayLong +
-                ", winningPercentLong=" + winningPercentLong + "\n" +
-                ", averageProfitShort=" + averageProfitShort +
-                ", averageHoldingPeriodShort=" + averageHoldingPeriodShort +
-                ", averageProfitPerDayShort=" + averageProfitPerDayShort +
-                ", winningPercentShort=" + winningPercentShort +
-                '}';
+        return "****** Average Stats ******\n" +
+                "Average Profit: " + averageProfit + "%" +
+                "\nAverage Holding Period: " + averageHoldingPeriod +
+                "\nAverage Profit Per Day:" + averageProfitPerDay + "%" +
+                "\nWinning Percent: " + winningPercent + "%\n\n" +
+                "****** Long Trade Stats ******\n" +
+                "Average Profit Long: " + averageProfitLong + "%" +
+                "\nAverage Holding Period Long: " + averageHoldingPeriodLong +
+                "\nAverage Profit Per Day Long: " + averageProfitPerDayLong + "%" +
+                "\nWinning Percent Long: " + winningPercentLong + "%\n\n" +
+                "****** Short Trade Stats ******\n" +
+                "Average Profit Short: " + averageProfitShort + "%" +
+                "\nAverage Holding Period Short: " + averageHoldingPeriodShort +
+                "\nAverage Profit Per Day Short: " + averageProfitPerDayShort + "%" +
+                "\nWinning Percent Short: " + winningPercentShort + "%";
     }
 }
